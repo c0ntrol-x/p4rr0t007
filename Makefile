3600e56e354b77cb33860936d3f1a39997d02322
Ð4√¡η¢Ч <d4v1ncy@protonmail.ch>
2017-02-26 09:43:27 -0500

    0.1.6

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 PACKAGE_NAME	:= p4rr0t007
-VERSION		:= 0.1.5
+VERSION		:= 0.1.6
 export VERSION
 export PACKAGE_NAME
 
--- tests/test_l33t.py
+++ tests/test_l33t.py
@@ -4,6 +4,9 @@ from __future__ import unicode_literals
 from p4rr0t007.lib import tr4nsl33t
 
 
-def test_transleet():
+def test_transleet_extended():
     tr4nsl33t('coOl direct translations', extended=True).should.equal('(001 |)!|23(7 7|24n5147!0n5')
+
+
+def test_transleet_ultimate():
     tr4nsl33t('coOl direct translations', ultimate=True).should.equal(u'\xa2\xf8\u03a9\u2113 \u03b4\xa1\u044f\u0259\xa2\u03c4 \u03c4\u044f\u03b1\u03b7$\u2113\u03b1\u03c4\xa1\xf8\u03b7$')
