7f58c431a18666745e7768fc4e0138016975d90b
Ð4√¡η¢Ч <d4v1ncy@protonmail.ch>
2017-02-26 09:38:26 -0500

    0.1.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ dependencies = filter(bool, map(bytes.strip, local_file('requirements.txt').spli
 # https://setuptools.readthedocs.io/en/latest/setuptools.html#adding-setup-arguments
 setup(
     name='p4rr0t007',
-    version='0.1.4',
+    version='0.1.5',
     description="\n".join([
         'h1gh lEv3l fl4sk 4 h4x0rs'
     ]),
